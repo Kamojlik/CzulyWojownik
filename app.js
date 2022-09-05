c7e5f9ff6cd9f107e59e646327fc53275cdfcf86
Kamojlik <samojlik.k@gmail.com>
2022-09-05 17:53:25 +0200

    dropdown menu

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 
 let hamburgerMenu = document.querySelector('.hamburger-menu');
-let navContent = document.querySelector('.nav-content');
+let dropdownMenu = document.querySelector('.dropdown-menu');
 
 hamburgerMenu.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
-	navContent.classList.toggle('change');
 	hamburgerMenu.classList.toggle('change');
+	dropdownMenu.classList.toggle('hide');
 }
+	
 	
